79c34ce4ee686dc361a6eaff4188f3f85a66cf3c
Mohamed Raafat <141199348+m7mdraafat@users.noreply.github.com>
2024-10-24 23:32:47 +0300

    Add ISpecification interface for applying specification design pattern.

---
+++ src/Infrastructure/FindIt.Persistence/Interfaces/ISpecification.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Linq.Expressions;
+
+namespace FindIt.Persistence.Interfaces
+{
+    public interface ISpecification<T> where T : class
+    {
+        Expression<Func<T, bool>> WhereCriteria { get; set; }
+        List<Expression<Func<T, object>>> InlcudesCriteria { get; set; }
+        Expression<Func<T, object>> OrderBy { get; set; }
+        Expression<Func<T, object>> OrderByDesc { get; set; }
+        
+
+        int Skip { get; set; }
+        int Take { get; set; }
+
+        bool IsPaginationEnabled { get; set; }
+
+    }
+}
