f8c8821f3ec04b7382c015c786505d7b18e054c5
Mohamed Raafat <141199348+m7mdraafat@users.noreply.github.com>
2024-10-25 00:49:01 +0300

    # Add Specification interface for help us for implementing a specification  design pattern

---
+++ src/Infrastructure/FindIt.Persistence/Interfaces/ISpecifications.cs
@@ -0,0 +1,9 @@
+﻿using System.Linq.Expressions;
+
+namespace FindIt.Persistence.Interfaces
+{
+    public interface ISpecifications<T> where T : class
+    {
+        Expression<Func<T, bool>> WhereCriteria { get; set; }
+    }
+}
