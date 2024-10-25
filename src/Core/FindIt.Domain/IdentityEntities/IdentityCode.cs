84b4bd56015914236860ef6162f004495700a5ae
Azza-shahen <azzashahen2004@gmail.com>
2024-10-26 01:16:19 +0300

    Implement  RefreshToken Class and IdentityCode Class

---
+++ src/Core/FindIt.Domain/IdentityEntities/IdentityCode.cs
@@ -0,0 +1,29 @@
+﻿
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using FindIt.Domain.Common;
+
+namespace FindIt.Domain.IdentityEntities
+{
+    public class IdentityCode : BaseEntity
+    {
+
+
+
+        public string Code { get; set; } = null!;
+
+        public bool IsActive { get; set; }
+
+        public bool ForRegistrationConfirmation { get; set; }
+
+        public DateTime CreationTime { get; set; }
+
+        public DateTime? ActivationTime { get; set; }
+
+        public int UserId { get; set; }
+
+        public virtual AppUser AppUser { get; set; } = null!;
+    }
+}
+
+
---
+++ src/Core/FindIt.Domain/IdentityEntities/RefreshToken.cs
@@ -0,0 +1,22 @@
+﻿using FindIt.Domain.Common;
+
+namespace FindIt.Domain.IdentityEntities
+{
+    public class RefreshToken : BaseEntity
+    {
+
+
+        public string Token { get; set; } = null!;
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime ExpiredAt { get; set; }
+
+        public DateTime? RevokedAt { get; set; }
+
+        public int UserId { get; set; }
+
+        public virtual AppUser AppUser { get; set; } = null!;
+
+    }
+}
