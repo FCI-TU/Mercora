7c28ffd40672a7deb42dd880af37887436d3888b
HasnaaHassan11 <165372872+HasnaaHassan11@users.noreply.github.com>
2024-10-25 22:54:20 +0300

    Implement UserAddress and AppUser classes for identity and address management

---
+++ src/Core/FindIt.Domain/AppUser.cs
@@ -0,0 +1,18 @@
+﻿
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace FindIt.Domain.IdentityEntities
+{
+    public class AppUser : IdentityUser
+    {
+
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public virtual ICollection<UserAddress> UserAddresses { get; set; } = null!;
+        //public virtual ICollection<IdentityCode> IdentityCodes { get; set; } = null!;
+        //public virtual ICollection<RefreshToken> RefreshTokens { get; set; } = null!;
+
+
+    }
+}
