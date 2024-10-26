22c3f822aaf09b393c571068a6cc11c74d90b992
Mohamed Raafat <141199348+m7mdraafat@users.noreply.github.com>
2024-10-26 15:33:19 +0300

    # register dependencies to IoC container. # add swagger middleware.

--- src/Presentation/FindIt.Server/Program.cs
+++ src/Presentation/FindIt.Server/Program.cs
@@ -1,34 +1,23 @@
+using FindIt.Server;
+using FindIt.Server.ServicesExtensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllersWithViews();
-builder.Services.AddRazorPages();
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.AddApplicationDependencies();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-    app.UseWebAssemblyDebugging();
-}
-
-app.UseHttpsRedirection();
-
-app.UseBlazorFrameworkFiles();
+app.UseSwaggerMiddleware();
 
 app.UseStaticFiles();
 
-app.UseAuthentication();
+app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapRazorPages();
 
 app.MapControllers();
 
-app.MapFallbackToFile("index.html");
 
 app.Run();
