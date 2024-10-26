d6023d46e38a807277700072b87a58f2e8fa8b02
Mohamed Raafat <141199348+m7mdraafat@users.noreply.github.com>
2024-10-26 14:41:35 +0300

    # Add ServiceExtensions static class to add services in an application layer. -> implement AddAuthenticationService that used for add any authentication that uses in our project such as (Oouth, Jwt Bearer, etc..), for now we implement Jwt Bearer only.

---
+++ src/Core/FindIt.Application/ServiceExtensions/ServiceExtensions.cs
@@ -0,0 +1,42 @@
+﻿
+using FindIt.Application.Settings;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace FindIt.Application.ServiceExtensions
+{
+    public static class ServiceExtensions
+    {
+        public static IServiceCollection AddAuthenticationService(this IServiceCollection services,
+            IOptions<JwtData> JwtData, IConfiguration configuration)
+        {
+
+            services.AddAuthentication(authenticationOptions =>
+            {
+                authenticationOptions.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                authenticationOptions.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                authenticationOptions.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(jwtBearerOptions =>
+            {
+                jwtBearerOptions.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateAudience = true,
+                    ValidAudience = JwtData.Value.ValidAudience,
+                    ValidateIssuer = true,
+                    ValidIssuer = JwtData.Value.ValidIssuer,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(JwtData.Value.SecretKey)),
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.Zero,
+                };
+            });
+
+            return services;
+        }
+    }
+}
