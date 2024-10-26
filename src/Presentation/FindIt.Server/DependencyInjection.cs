58c079c2a86e4419d93e17566805e0804b06716e
Mohamed Raafat <141199348+m7mdraafat@users.noreply.github.com>
2024-10-26 15:21:21 +0300

    # Add Dependency injection class to register all services on it to keep the program.cs more cleaner.

---
+++ src/Presentation/FindIt.Server/DependencyInjection.cs
@@ -0,0 +1,32 @@
+﻿using FindIt.Application.ServiceExtensions;
+using FindIt.Application.Settings;
+using FindIt.Server.ServicesExtensions;
+using Microsoft.Extensions.Options;
+
+namespace FindIt.Server
+{
+    public static class DependencyInjection
+    {
+        public static WebApplicationBuilder AddApplicationDependencies(this WebApplicationBuilder builder)
+        {
+            var services = builder.Services;
+            var configuration = builder.Configuration;
+            var environment = builder.Environment;
+
+            services.ConfigureAppSettingsData(configuration);
+
+            var servicesProvider = services.BuildServiceProvider();
+
+            var jwtData = servicesProvider.GetRequiredService<IOptions<JwtData>>().Value;
+
+            services.AddControllers();
+
+            services.AddSwaggerServices();
+
+            services.AddAuthenticationService(jwtData, configuration);
+
+
+            return builder;
+        }
+    }
+}
