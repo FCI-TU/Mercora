be7e2295f48d1a7be6fddbd94dc92a1c6b5a4bf2
AhmedGamal-Cs <asd.7shaltwatz@gmail.com>
2024-10-26 10:34:32 -0700

    add DbContext Class

---
+++ src/Infrastructure/FindIt.Persistence/Context/StoreDbContext.cs
@@ -0,0 +1,32 @@
+﻿using FindIt.Domain.IdentityEntities;
+using FindIt.Persistence.Context.Configurations;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace FindIt.Persistence.Context
+{
+    public class StoreDbContext : IdentityDbContext<AppUser>
+    {
+        public StoreDbContext(DbContextOptions<StoreDbContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(typeof(AppUserConfigurations).Assembly);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        public DbSet<AppUser> AppUsers { get; set; }
+        public DbSet<IdentityCode> IdentityCodes { get; set; }
+        public DbSet<RefreshToken> RefreshTokens { get; set; }
+        public DbSet<UserAddress> UserAddresses { get; set; }
+
+    }
+}
