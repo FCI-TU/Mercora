3edcf3fdeacc1562c112f7e9cb08fe3de299e456
IBruteDude <kamarf505@gmail.com>
2024-10-29 23:11:48 +0300

    Added ErrorHandling types

---
+++ src/Core/FindIt.Application/ErrorHandling/Result.cs
@@ -0,0 +1,39 @@
+﻿namespace FindIt.Application.ErrorHandling;
+public class Result<T>
+{
+	private T? _data { get; set; }
+	private Status? _error { get; set; }
+
+
+	public Result(T data, Status error)
+	{
+		_data = data;
+		_error = error;
+	}
+	public Result(T data)
+	{
+		_data = data;
+		_error = null;
+	}
+
+	public Result(Status error)
+	{
+		_data = default;
+		_error = error;
+	}
+
+	T GetValue()
+	{
+		return _data;
+	}
+}
+
+public class Result : Result<string>
+{
+	public Result(string data) : base(data)
+	{
+	}
+	public Result(Status error) : base(error)
+	{
+	}
+}
