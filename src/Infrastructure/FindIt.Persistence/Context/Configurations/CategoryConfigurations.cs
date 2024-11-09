59012272b36545464cf89e54e2697ff58e681d41
HasnaaHassan11 <165372872+HasnaaHassan11@users.noreply.github.com>
2024-11-09 18:50:10 +0200

    added the configurations of category

---
+++ src/Infrastructure/FindIt.Persistence/Context/Configurations/CategoryConfigurations.cs
@@ -0,0 +1,27 @@
+﻿using FindIt.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FindIt.Persistence.Context.Configurations
+{
+    internal class CategoryConfigurations : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.Name)
+                   .IsRequired()
+                   .HasMaxLength(50);
+
+            builder.Property(c => c.Description)
+                   .IsRequired()
+                   .HasMaxLength(200);
+
+            builder.Property(c => c.CoverImageUrl)
+                   .IsRequired();
+
+           
+        }
+    }
+}
