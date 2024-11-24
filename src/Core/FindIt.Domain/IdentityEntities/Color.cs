ee48b06df8741df52215feb58374c354a2acb7e8
AhmedM7med <ahmedelgebaly@gmail.com>
2024-11-24 15:51:22 +0200

    Add Product, ProductColor, Color Entities and Configuration of Product Entity Only.

---
+++ src/Core/FindIt.Domain/IdentityEntities/Color.cs
@@ -0,0 +1,11 @@
+﻿using FindIt.Domain.Common;
+
+namespace FindIt.Domain.IdentityEntities
+{
+    public class Color:BaseEntity
+    {
+        public string HexCode { get; set; } = null!;
+        public string Name { get; set; } = null!;
+        public virtual ProductColor ProductColor { get; set; } = null!;
+    }
+}
---
+++ src/Infrastructure/FindIt.Persistence/Context/Configurations/ProductConfigurations.cs
@@ -0,0 +1,43 @@
+﻿using FindIt.Domain.IdentityEntities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FindIt.Persistence.Context.Configurations
+{
+    class ProductConfigurations : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            // Primary Key
+            builder.HasKey(p => p.Id);
+
+            // Property Configurations
+            builder.Property(p => p.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(p => p.Description)
+                .HasMaxLength(255);
+
+            builder.Property(p => p.Price)
+                .HasColumnType("decimal(18,2)");
+
+            builder.Property(p => p.Status)
+                .HasMaxLength(50);
+
+            // Relationship With Category
+            //builder.HasOne(p => p.Category)
+            //    .WithMany(c => c.Product)
+            //    .HasForeignKey(p => p.CategoryId)
+            //    .OnDelete(DeleteBehavior.Restrict);
+
+            // Relationship With Brand
+            //builder.HasOne(p => p.Brand)
+            //  .WithMany(b => b.Product)
+            //  .HasForeignKey(p => p.BrandId)
+            //  .OnDelete(DeleteBehavior.Restrict);
+
+
+        }
+    }
+}
