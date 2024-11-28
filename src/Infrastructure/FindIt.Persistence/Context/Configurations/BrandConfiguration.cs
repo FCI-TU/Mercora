1b9be240a4649cfc0fe612296dd362bd973f1c87
Azza-shahen <azzashahen2004@gmail.com>
2024-11-28 14:38:08 +0200

    Add Brand Entity and its Configuration

---
+++ src/Infrastructure/FindIt.Persistence/Context/Configurations/BrandConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using FindIt.Domain.IdentityEntities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FindIt.Persistence.Context.Configurations
+{
+    public class BrandConfiguration : IEntityTypeConfiguration<Brand>
+    {
+        public void Configure(EntityTypeBuilder<Brand> builder)
+        {
+            builder.Property(p => p.Name)
+           .IsRequired()
+           .HasMaxLength(100);
+
+            builder.Property(p => p.Description)
+                .IsRequired()
+           .HasMaxLength(100);
+            builder.Property(p => p.LogUrl)
+                .IsRequired(false)
+           .HasMaxLength(100);
+        }
+
+    }
+}
+
