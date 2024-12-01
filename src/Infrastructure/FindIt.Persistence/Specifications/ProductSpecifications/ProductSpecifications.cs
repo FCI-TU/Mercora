35d3aed0ad428f853c7ad06583f492b3fe967010
Mohamed Raafat <141199348+m7mdraafat@users.noreply.github.com>
2024-12-01 18:28:17 +0200

    # Implement Product Specification to allow us for searching, sorting all products and get their related data such as category, brand, and other properties.

---
+++ src/Infrastructure/FindIt.Persistence/Specifications/ProductSpecifications/ProductSpecifications.cs
@@ -0,0 +1,20 @@
+﻿namespace FindIt.Persistence.Specifications.ProductSpecifications
+{
+    public class ProductSpecifications
+    {
+        private const int MaxPageSize = 10;
+        private int _PageSize = 10;
+
+        public int PageIndex = 1; 
+        public int PageSize
+        {
+            get => _PageSize;
+            set => _PageSize = value > MaxPageSize ? _PageSize : value;
+        }
+
+        public int? CategoryId { get; set; }
+        public int? BrandId { get; set; }
+        public string? Sort { get; set; }
+        public string? Search {  get; set; }
+    }
+}
