80beac126bd6e5d99a8b2892de82b4df82ba6d00
Mohamed Raafat <141199348+m7mdraafat@users.noreply.github.com>
2024-12-01 19:31:50 +0200

    # Add category service interface. # Add Product Service interface. # Add Brand Service interface.

---
+++ src/Core/FindIt.Application/Interfaces/IProductService.cs
@@ -0,0 +1,18 @@
+﻿
+
+using FindIt.Application.DTOs;
+using FindIt.Application.ErrorHandling;
+using FindIt.Domain.Specifications.ProductSpecifications;
+using FindIt.Shared.DTOs;
+
+namespace FindIt.Application.Interfaces
+{
+    public interface IProductService
+    {
+        Task<Result<Pagination<ProductResponse>>> GetAllProductsAsync(ProductSpecifications productSpecifications);
+        Task<Result<ProductResponse>> GetProductAsync(int productId);
+        Task<Result<ProductResponse>> CreateProductAsync(ProductRequest productRequest);
+        Task<Result<ProductResponse>> UpdateProductAsync(int productId, ProductRequest productRequest);
+        Task<Result<string>> DeleteProductAsync(int productId);
+    }
+}
