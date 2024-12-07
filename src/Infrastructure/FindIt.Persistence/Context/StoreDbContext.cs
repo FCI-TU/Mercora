b09e21281abc1677acc208e2b6ac741dafda40e4
AhmedM7med <ahmedelgebaly2511@gmail.com>
2024-12-08 01:22:33 +0200

    Add Cart Entity and Its Configurations

--- src/Infrastructure/FindIt.Persistence/Context/StoreDbContext.cs
+++ src/Infrastructure/FindIt.Persistence/Context/StoreDbContext.cs
@@ -1,4 +1,5 @@
-﻿using FindIt.Domain.IdentityEntities;
+﻿using FindIt.Domain.CartEntities;
+using FindIt.Domain.IdentityEntities;
 using FindIt.Domain.ProductEntities;
 using FindIt.Persistence.Context.Configurations;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
@@ -19,4 +20,6 @@ public class StoreDbContext(DbContextOptions<StoreDbContext> options) : Identity
     public DbSet<Product> Products { get; set; }
     public DbSet<Brand> Brands { get; set; }
     public DbSet<Category> Categories { get; set; }
+    public DbSet<Cart> Carts { get; set; }
+
 }
