c43366f6f894a7b901151da0eaf824e0ae674ba8
Ahmed Eldamity <ahmedeldamity25@gmail.com>
2024-12-12 20:45:20 +0200

    Add Order Migrations

--- src/Core/FindIt.Domain/OrderEntities/OrderItem.cs
+++ src/Core/FindIt.Domain/OrderEntities/OrderItem.cs
@@ -3,7 +3,23 @@
 namespace FindIt.Domain.OrderEntities;
 public class OrderItem : BaseEntity
 {
-    public ProductOrderItem Product { get; set; } = null!;
-    public decimal Price { get; set; }
+	public OrderItem() { /* we create this constructor because EF need it while migration to make instance from this class */ }
+	public OrderItem(ProductOrderItem product, decimal price, decimal quantity)
+	{
+		Product = product;
+		Price = price;
+		Quantity = quantity;
+	}
+
+	public ProductOrderItem Product { get; set; } = null!;
+	#region explain
+	// this is a navigation property
+	// so EF will mapped it to database
+	// but we don't need that
+	// we need take his properties and mapped it in OrderItem table
+	// so will make configration for that :)
+	#endregion
+
+	public decimal Price { get; set; }
     public decimal Quantity { get; set; }
 }
